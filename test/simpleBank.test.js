1c433dfaa64e1efb5f9723003dfd31d64404b3ee
hlchia <chiahl2@gmail.com>
2019-01-27 15:32:58 +0800

    Update simpleBank.test.js

--- test/simpleBank.test.js
+++ test/simpleBank.test.js
@@ -5,7 +5,7 @@ contract('SimpleBank', function(accounts) {
   const owner = accounts[0]
   const alice = accounts[1];
   const bob = accounts[2];
-  const deposit = web3.toBN(2);
+  const deposit = web3.toBigNumber(2);
 
   it("mark addresses as enrolled", async () => {
     const bank = await SimpleBank.deployed();
